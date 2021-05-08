266da86c61446898475b924473e04ae9c53ba481
MasonChance <mason.fryberger@gmail.com>
2021-05-08 14:57:37 -0700

    added multiple models.

---
+++ grading_app/models.py
@@ -0,0 +1,110 @@
+from django.db import models
+
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+
+class Student(models.Model):
+    # student_id pulled from Canvas_API
+    student_id = models.IntegerField()
+    # first_name from Canvas_API based on Student_ID
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    # class_key from Canvas_API (student instances should only be
+    # created for students whose class_code are equivalant to "401")
+    # if a class is somehow deleted, class_key will be set to None. implement
+    class_key = models.ForeignKey('ClassList', to_field='class_code')
+    instructor_key = models.ForeignKey('Instructors',to_field='instructor_id')
+    # class_hist is a list of class_id instances that the Student has been enrolled in.
+    class_hist = models.JSONField()
+    scheduled = models.BooleanField(default=False)
+    attempts = models.IntegerField()
+    email = models.URLField()
+
+
+
+
+
+    def __str__(self):
+        return self.first_name + self.last_name + self.student_id
+
+
+    # method to check if student is scheduled
+    def _is_scheduled__(self):
+        pass
+
+    # method fetches data from db regarding all wb records where student_key and
+    # and class_key match student record student_id and class_key (composite key??)
+    def _get_history(self):
+        pass
+    
+
+
+class ClassList(models.Model):
+    class_code = models.CharField(max_len=64)
+    instructor_key = models.ForeignKey('Instructors', to_field='instructor_id')
+
+
+
+
+    def __str__(self):
+        return str(self.class_code)
+    
+
+    def __repr__(self):
+        return self.pk
+
+    
+
+
+
+class Instructors(models.Model):
+    # TODO: determine if we should use default pk or assign pk based on Canvas info. 
+    instructor_id = models.IntegerField()
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    # pulled from Slack upon first sign-in
+    email = models.URLField()
+    # we may need to add a dropdown to the my_class_view allowing the instructor to select
+    # which of their classes they wish to view, unless the Canvas info has an indicator of 
+    # which class is currently  assigned to the instructor
+    class_key = models.ForeignKey('ClassList', to_field='class_code')
+
+
+
+
+    def __str__(self):
+        return str(self.first_name + self.last_name + self.instructor_id)
+
+
+
+class Whiteboard(models.Model):
+    student_key = models.ForeignKey('Student', to_field='student_id')
+    interviewer_key = models.ForeignKey('Instructors', to_field='instructor_id' )
+    problem_domain_key = models.ForeignKey('Problemdomain', to_field='domain_url')
+    category_notes_key = models.ForeignKey('CategoryNotes', on_delete=models.CASCADE)
+    score_key = models.ForeignKey('ScoreTable', on_delete=models.CASCADE)
+    wb_image = models.ForeignKey('WhiteboardImage', on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    passed = models.BooleanField(default=False)
+    
+
+
+    def __str__(self):
+        return str(self.problem_domain_key + self.date + self.student_key + self.passed)
+
+
+    # checks if whiteboard is pending or complete. returns ---> "pending" + self.date
+    def _status(self):
+        pass
+
+
+    
+
+    
+    
+
+
+
+
